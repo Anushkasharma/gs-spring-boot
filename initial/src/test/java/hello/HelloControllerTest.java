77e5614e244a53001174562726541182d48081df
Anushka Sharma <asharma2@wayfair.com>
2019-05-28 09:58:49 -0400

    Make test pass

--- initial/src/test/java/hello/HelloControllerTest.java
+++ initial/src/test/java/hello/HelloControllerTest.java
@@ -25,7 +25,7 @@ public class HelloControllerTest {
     @Test
     public void getHello() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isNotFound())
+                .andExpect(status().isOk())
                 .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
     }
 }
